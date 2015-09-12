085d6c7dcc5e0cb0cd66d0c384aefe9d7efe2f30
whuang8 <huang554@purdue.edu>
2015-09-12 13:43:02 -0400

    first commit

--- HackaTeam/ViewController.swift
+++ HackaTeam/ViewController.swift
@@ -6,13 +6,37 @@
 //  Copyright (c) 2015 William Huang. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
 
+    @IBAction func signUp(sender: UIButton) {
+        var ref = Firebase(url:"https://hackateam.firebaseio.com/")
+        ref.createUser(email.text, password: password.text,
+            withValueCompletionBlock: { error, result in
+                if error != nil {
+                    // There was an error creating the account
+                } else {
+                    let uid = result["uid"] as? String
+                    println("Successfully created user account with uid: \(uid)")
+                }
+        })
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myRootRef = Firebase(url:"https://hackateam.firebaseio.com/")
+        var alanisawesome = ["full_name": "Alan Turing", "date_of_birth": "June 23, 1912"]
+        var gracehop = ["full_name": "Grace Hopper", "date_of_birth": "December 9, 1906"]
+        
+        var usersRef = myRootRef.childByAppendingPath("users")
+        
+        var users = []
+        usersRef.setValue(users)
     }
 
     override func didReceiveMemoryWarning() {
