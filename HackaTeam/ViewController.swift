07cd448343ea1254d5198f666c916526f49a26fa
whuang8 <huang554@purdue.edu>
2015-09-13 03:33:07 -0400

    login feature works. Error handling works. :D

--- HackaTeam/ViewController.swift
+++ HackaTeam/ViewController.swift
@@ -12,28 +12,44 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var error: UILabel!
 
+    var loggedIn: Bool = false
+    
+    @IBAction func signIn(sender: UIButton) {
+        let ref = Firebase(url: "https://hackateam.firebaseio.com/")
+        ref.authUser(email.text, password: password.text,
+            withCompletionBlock: { error, authData in
+                if error != nil {
+                    // There was an error logging in to this account
+                    println("Error in log in")
+                    self.error.hidden = false
+                    self.email.text = ""
+                    self.password.text = ""
+                    self.loggedIn = false
+                } else {
+                    // We are now logged in
+                    println("Log in successful")
+                    self.loggedIn = true
+                    self.performSegueWithIdentifier("SignInSegue", sender: self)
+                }
+        })
 
-//    @IBAction func signIn(sender: UIButton) {
-//        let ref = Firebase(url: "https://hackateam.firebaseio.com/")
-//        ref.authUser(email.text, password: password.text,
-//            withCompletionBlock: { error, authData in
-//                if error != nil {
-//                    // There was an error logging in to this account
-//                    
-//
-//                } else {
-//                    // We are now logged in
-//                    print("Log in successful")
-//                }
-//        })
-//    }
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+    
+        let performSegue = self.loggedIn
+        self.loggedIn = false
+        return performSegue
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
         view.addGestureRecognizer(tap)
+        
     }
     
     func DismissKeyboard(){
