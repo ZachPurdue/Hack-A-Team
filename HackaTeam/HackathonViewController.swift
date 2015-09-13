c03e8b1ffce59a74384ea701fe1ef563ec145b80
whuang8 <huang554@purdue.edu>
2015-09-12 22:01:47 -0400

    add hackathonViewController

---
+++ HackaTeam/HackathonViewController.swift
@@ -0,0 +1,60 @@
+//
+//  HackathonViewController.swift
+//  HackaTeam
+//
+//  Created by William Huang on 9/12/15.
+//  Copyright (c) 2015 William Huang. All rights reserved.
+//
+
+import Firebase
+import UIKit
+
+class HackathonViewController: UIViewController {
+    
+    @IBAction func mhacks(sender: UIButton) {
+        
+    }
+
+    @IBAction func hackTheNorth(sender: UIButton) {
+        
+    }
+    
+    @IBAction func hackRU(sender: UIButton) {
+        
+    }
+    
+    @IBAction func vandyHacks(sender: UIButton) {
+        
+    }
+    
+    @IBAction func boilerMake(sender: UIButton) {
+        
+    }
+    
+    @IBAction func wildHacks(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
